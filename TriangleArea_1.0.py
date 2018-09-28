05428960c9ee6b66e5ee612360e2f35e413b94ba
Aaspyr <aaspyr@gmail.com>
2018-09-28 12:53:57 +0200

    Initial commit

---
+++ TriangleArea_1.0.py
@@ -0,0 +1,11 @@
+" Program return us an area of the triangle"
+def view(score):
+    print(score)
+    return
+
+def triangleArea(a,h):
+    return view(0.5*a*h)
+
+a = int(input("Write base: "))
+h = int(input("Write height: "))
+triangleArea(a, h)
